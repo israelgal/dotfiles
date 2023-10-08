97d5a2e964b947fd90ec869a771fdd619f7f8640
israelgal <israelgalrod@gmail.com>
2023-10-08 11:49:10 -0600

    more changes

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -64,23 +64,36 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
+
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
+
+
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
+
+
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
+
+
     Key(
         [mod, "shift"],
         "Return",
@@ -91,7 +104,7 @@ keys = [
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
@@ -130,6 +143,11 @@ layout_theme = {
         "border_normal":"#cba6f7"
 }
 
+Catppuccin = [["#a6e3a1", "#a6e3a1"],
+            ["#cba6f7", "#cba6f7"],
+            ['#313244', '#313244']]
+
+
 layouts = [
     #layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     #layout.Max(),
@@ -157,26 +175,30 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                #widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                #widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                #widget.TextBox("default config", name="default"),
-                #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
                 widget.Spacer(),
+
+                widget.GroupBox(
+                    highlight_method = 'block',
+                    block_highlight_text_color = 'ffffff',#Catppuccin[1],
+                    disable_drag = True,
+                    inactive = 'ffffff',
+                    rounded = False,
+                    active = Catppuccin[1],
+                    this_current_screen_border = Catppuccin[0]
+                    ),
+
+
+                widget.Spacer(),
+
                 widget.Systray(),
+
                 widget.Clock(format="%a %I:%M %p"),
-                widget.QuickExit(),
+
+
             ],
-            24,
+            size = 24,
+            background = Catppuccin[2]
+            
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
